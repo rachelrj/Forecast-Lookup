8bc7ef56b70ae6d3afb27877355a04bb313e9521
Rachel <rachelrj@umich.edu>
2015-04-12 00:38:31 -0400

    Initial commit!
    Add php file which allows user to enter location
    Add react library files

---
+++ weather-app.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+	<head>
+ 		<title>Weather Lookup | Rachel R Joyce</title>
+
+		<meta HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=iso-8859-1">
+		<meta name="Author" content="Rachel R Joyce">
+		<meta http-equiv="Content-Language" content="en-us">
+		<meta NAME="GOOGLEBOT" CONTENT="INDEX, FOLLOW">
+		<meta name="robots" content="all, index, follow">
+		<meta name="rating" content="general">
+		<meta name="viewport" content="initial-scale=1">
+
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+		<script src="build/react.js"></script>
+    	<script src="build/JSXTransformer.js"></script>
+
+		<script>
+
+			//call this function when user submits location
+			function submitLocation() {
+				var userresponse = document.getElementById('location-entry').value;
+				document.getElementById("response").innerHTML = '<p>' + userresponse + '</p>';
+			}
+
+		</script>
+
+	</head>
+
+	<body>
+
+		<h1>
+			Weather Lookup
+		</h1>
+
+		<p> 
+			Please enter your location below to lookup the weather in your city
+		</p>
+
+		<form onsubmit="submitLocation()" method="post">
+  				<input id="location-entry" name="location-entry" placeholder="Enter a city" type="text">
+ 				<input type="submit" value="Send">
+		</form>
+
+		<div id="response">
+		</div>
+
+	</body>
+
+</html>
