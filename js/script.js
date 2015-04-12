9877fe9dc6a98e6fbd13ac3e3bc0da382dafce9b
Rachel <rachelrj@umich.edu>
2015-04-12 19:02:33 -0400

    Move javascript into new js file

---
+++ js/script.js
@@ -0,0 +1,30 @@
+//Autocomplete function
+$(function() {
+	var returnedLocations = [
+			"ActionScript",
+			"AppleScript",
+			"Asp",
+			"BASIC",
+			"C",
+			"C++",
+			"Clojure",
+			"COBOL",
+			"ColdFusion",
+			"Erlang",
+			"Fortran",
+			"Groovy",
+			"Haskell",
+			"Java",
+			"JavaScript",
+			"Lisp",
+			"Perl",
+			"PHP",
+			"Python",
+			"Ruby",
+			"Scala",
+			"Scheme"
+		];
+	$( "#location-entry" ).autocomplete({
+		source: returnedLocations
+	});
+});
