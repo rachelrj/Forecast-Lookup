80caaa5d715a487d5cda50507b7ed61fcdedea24
Rachel <rachelrj@umich.edu>
2015-04-14 03:29:28 -0400

    clean up code

--- js/script.js
+++ js/script.js
@@ -1,19 +1,16 @@
 //Use weather underground api to populate autocomplete options
 function autoCompleteFunction() {
 
-	var userResponse = $( '#location-entry').val();
-	var returnedLocations = [];
-
 	$.ajax({
 				url : 'http://autocomplete.wunderground.com/aq',
 				dataType: "jsonp", 
 				jsonp:    "cb",
     			data:     {
         			format: "json",
-        			query:  userResponse
+        			query:  $( '#location-entry').val()
     			},
     			success:  function (data) {
-        			var i;
+        			
         			var returnedLocations = [];
         			for (i in data.RESULTS) {
         				returnedLocations.push(data.RESULTS[i].name);
