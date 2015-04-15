7e3aaa21f965b2869a42905ab458da02b17f5f63
Rachel <rachelrj@umich.edu>
2015-04-15 01:41:36 -0400

    On user submit, use wunderground forecast api method to display 3-day weather forecast on screen

--- js/script.js
+++ js/script.js
@@ -20,6 +20,55 @@ function autoCompleteFunction() {
 						delay: 500
 					});
     			}
-});
+    });
+};
 
+//get weather from user's query with wunderground forecast api method
+function getWeather(q) {
+
+    $.ajax({
+                url : 'http://api.wunderground.com/api/362c5ac3fc63dd76/forecast' + q,
+                dataType: "json",
+                jsonp:    "cb",
+                data:       {
+                    format: "json",
+                },
+                success:  function (data) {
+                    //store weather                    
+                    var date = [];
+                    var weather = [];
+                    var description = [];
+                    for (i in data.forecast.txt_forecast.forecastday) {
+                        date.push(data.forecast.txt_forecast.forecastday[i].title);
+                        weather.push(data.forecast.txt_forecast.forecastday[i].icon);
+                        description.push(data.forecast.txt_forecast.forecastday[i].fcttext);
+                    }
+                    //print weather to page
+                    for(i = 0; i < 8; i++)
+                    {
+                        $('<h2>'+ date[i] + '</h2>').appendTo('#response');
+                        $('<p>' + description[i] + '</p>').appendTo('#response');
+                    }
+                }
+    });
+};
+
+function submitLocation() {
+
+    //remove previous response
+    $( "#response" ).empty();
+
+    //get query from autocomplete api
+    $.ajax({
+                url : 'http://autocomplete.wunderground.com/aq',
+                dataType: "jsonp", 
+                jsonp:    "cb",
+                data:     {
+                    format: "json",
+                    query:  $( '#location-entry').val()
+                },
+                success:  function (data) {
+                        getWeather(data.RESULTS[0].l + '.json');
+                }
+    });
 };
