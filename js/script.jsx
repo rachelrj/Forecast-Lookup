e8dd4e6fd180a8f0aa72a5f0258be21e01e49eff
Rachel <rachelrj@umich.edu>
2015-04-19 02:11:00 -0400

    Remove two more unneccesary states from react class. Refactor previousDay function and nextDay function.

--- js/script.jsx
+++ js/script.jsx
@@ -9,38 +9,24 @@ propTypes:{
 },
 getInitialState:function(){
   return{
-      displayPrevious : false,
-      displayNext : true,
       day: 0
   }
 },
 previousDay: function(e){
-  var previous = this.state.day -1;
-  var display = true;
-  if(previous <1)
-    display = false;
   this.setState({
-    displayNext : true,
-    displayPrevious : display,
-    day : previous
+    day : this.state.day - 1
   });
 },
 nextDay: function(e){
-  var next = this.state.day +1;
-  var display = true;
-  if(next >6)
-    display = false;
   this.setState({
-    displayPrevious : true,
-    displayNext : display,
-    day : next
+    day : this.state.day + 1
   });
 },
 render:function(){
   return (
     <div>
-        <input className = {this.state.displayPrevious ? '' : 'hiddenButton'} type = "button" value = "previous" id= "previous" onClick={this.previousDay}/>
-        <input className = {this.state.displayNext ? '' : 'hiddenButton'} type = "button" value = "next" id = "next" onClick={this.nextDay}/>
+        <input className = {(this.state.day > 0) ? '' : 'hiddenButton'} type = "button" value = "previous" id= "previous" onClick={this.previousDay}/>
+        <input className = {(this.state.day < 7) ? '' : 'hiddenButton'} type = "button" value = "next" id = "next" onClick={this.nextDay}/>
       <h2> {this.props.weather[this.state.day].date} </h2>
       <p> {this.props.weather[this.state.day].longDescription} </p>
     </div>
