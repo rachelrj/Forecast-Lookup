d91a45542fe8d06127f044fd56371433100d650a
Rachel <rachelrj@umich.edu>
2015-04-19 15:48:05 -0400

    display day and night in same result

--- js/script.jsx
+++ js/script.jsx
@@ -14,21 +14,27 @@ getInitialState:function(){
 },
 previousDay: function(e){
   this.setState({
-    day : this.state.day - 1
+    day : this.state.day - 2
   });
 },
 nextDay: function(e){
   this.setState({
-    day : this.state.day + 1
+    day : this.state.day + 2
   });
 },
 render:function(){
   return (
     <div>
-        <input className = {(this.state.day > 0) ? '' : 'hiddenButton'} type = "button" value = "previous" id= "previous" onClick={this.previousDay}/>
-        <input className = {(this.state.day < 7) ? '' : 'hiddenButton'} type = "button" value = "next" id = "next" onClick={this.nextDay}/>
-      <h2> {this.props.weather[this.state.day].date} </h2>
-      <p> {this.props.weather[this.state.day].longDescription} </p>
+      <input className = {(this.state.day > 0) ? '' : 'hiddenButton'} type = "button" value = "previous" id= "previous" onClick={this.previousDay}/>
+      <input className = {(this.state.day < 6) ? '' : 'hiddenButton'} type = "button" value = "next" id = "next" onClick={this.nextDay}/>
+      <div id = 'day'>
+        <h2> {this.props.weather[this.state.day].date} </h2>
+        <p> {this.props.weather[this.state.day].longDescription} </p>
+      </div>
+      <div id = 'night'>
+        <h2> {this.props.weather[this.state.day+1].date} </h2>
+        <p> {this.props.weather[this.state.day+1].longDescription} </p>
+      </div>
     </div>
     );
 }
